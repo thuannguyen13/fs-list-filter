deed26e0fbaf420405ef1cfc2cf9097947559a3b
Thuan Nguyen <thuannguyen@Thuans-MacBook-Pro.local>
2026-03-19 23:04:02 -0500

    fix: bootstrap window.FinsweetAttributes before init

--- src/attributes.ts
+++ src/attributes.ts
@@ -1,3 +1,18 @@
-import { init } from '@finsweet/attributes-list';
+import { init } from '../packages/list/src/init';
+
+// Bootstrap window.FinsweetAttributes so internal helpers can iterate
+// window.FinsweetAttributes.scripts to read global settings from the <script> tag.
+window.FinsweetAttributes = Object.assign(
+  {
+    scripts: [...document.querySelectorAll<HTMLScriptElement>('script[fs-list]')],
+    modules: {},
+    process: new Set(),
+    version: '2.0.0',
+    push: () => {},
+    load: () => undefined,
+    utils: {},
+  },
+  window.FinsweetAttributes ?? {}
+);
 
 init();
